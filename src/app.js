cb1da8e9b22fd5e036880ecaf43041b4f2e5a584
JordiVF <jordivarofernandez@gmail.com>
2026-01-31 19:08:14 +0100

    Creada la ruta GET a supabase

--- src/app.js
+++ src/app.js
@@ -2,16 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const productosRoutes = require('./routes/productosRoutes');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/productos', productosRoutes);
 
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Servidor arrancado');
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
