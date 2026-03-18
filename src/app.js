119903bb70a852e10f8bb6873c2b9a3a3ab5687c
JordiVF <jordivarofernandez@gmail.com>
2026-03-18 20:46:32 +0100

    CRUD de Usuarios completo

--- src/app.js
+++ src/app.js
@@ -3,15 +3,17 @@ const cors = require('cors');
 require('dotenv').config();
 
 const productosRoutes = require('./routes/productosRoutes');
+const usuariosRoutes = require('./routes/usuariosRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/productos', productosRoutes);
+app.use('/api/usuarios', usuariosRoutes);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
