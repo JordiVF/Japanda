d2ad6fca70db256ba0503bc99f1e743df62f8e10
JordiVF <jordivarofernandez@gmail.com>
2026-05-14 20:46:54 +0200

    Añadiendo logs de llamadas a API de categorias

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// esto es para ver la categoria del producto, a lo mejor lo quitamos
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.use('/api/productos', productosRoutes);
 app.use('/api/usuarios', usuariosRoutes);
 app.use('/api/categorias', categoriasRoutes);
