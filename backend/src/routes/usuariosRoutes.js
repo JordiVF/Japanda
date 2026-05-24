e29d42b429582111b4f92fa659e549263a204e02
JordiVF <jordivarofernandez@gmail.com>
2026-05-24 15:04:18 +0200

    cambiado rutas de usuario

--- backend/src/routes/usuariosRoutes.js
+++ backend/src/routes/usuariosRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuariosController');
-const { verificarPropietario } = require('../middlewares/proteccionMiddleware');
+const { verificarUsuarioPorEmail, verificarPropietario } = require('../middlewares/proteccionMiddleware');
 
 router.get('/', usuariosController.getUsuarios);
 router.get('/:id', usuariosController.getUsuarioById);
@@ -9,6 +9,6 @@ router.post('/', usuariosController.createUsuario);
 router.post('/login', usuariosController.loginUsuario);
 router.put('/:id', verificarPropietario, usuariosController.updateUsuario);
 router.post('/:id/change-password', verificarPropietario, usuariosController.changePassword);
-router.delete('/:id', usuariosController.deleteUsuario);
+router.delete('/:id', verificarPropietario, usuariosController.deleteUsuario);
 
 module.exports = router;
