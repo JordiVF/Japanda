c3405111f351831ab3d6f0320abd0246afa7fb81
JordiVF <jordivarofernandez@gmail.com>
2026-05-24 20:04:49 +0200

    modificando orden de rutas para evitar errores

--- backend/src/routes/pedidosRoutes.js
+++ backend/src/routes/pedidosRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const pedidosController = require('../controllers/pedidosController');
 
 router.get('/', pedidosController.getPedidos);
-router.get('/:id', pedidosController.getPedidoById);
 router.get('/usuario/:id_usuario', pedidosController.getPedidosByUsuario);
+router.get('/:id', pedidosController.getPedidoById);
 router.post('/', pedidosController.createPedido);
 router.put('/:id', pedidosController.updatePedido);
 router.delete('/:id', pedidosController.deletePedido);
