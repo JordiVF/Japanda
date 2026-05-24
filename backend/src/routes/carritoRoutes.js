7ad97836172bcd1ee8c1341f599c276a56b58038
JordiVF <jordivarofernandez@gmail.com>
2026-05-24 16:07:57 +0200

    Arreglando bug carritoRoutes

--- backend/src/routes/carritoRoutes.js
+++ backend/src/routes/carritoRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const carritoController = require('../controllers/carritoController');
 
 router.get('/', carritoController.getTodosLosCarritos);
-router.get('/usuario/:id_usuario', autenticarPorIdUsuario, carritoController.getCarritoUsuario);
+router.get('/usuario/:id_usuario', carritoController.getCarritoUsuario);
 router.get('/:id', carritoController.getCarritoById);
 router.post('/', carritoController.createCarrito);
 router.put('/:id_usuario/:id', carritoController.updateCarrito);
